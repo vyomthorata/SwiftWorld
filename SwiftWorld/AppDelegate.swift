6a86428d34588da79ec44bff7e0ad1a3a7dd7fbd
Suresh <suresh.thorata@gmail.com>
2018-02-28 22:12:59 +0530

    New UI button action.

--- SwiftWorld/AppDelegate.swift
+++ SwiftWorld/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Application did finish launching")
+        FirebaseApp.configure()
         return true
     }
 
--- SwiftWorld/ViewController.swift
+++ SwiftWorld/ViewController.swift
@@ -17,11 +17,16 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func buttonClicked(_ sender: UIButton){
+        print("This is a Git tutorial")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
 
