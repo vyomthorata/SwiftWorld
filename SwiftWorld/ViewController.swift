5e67c2ed36748d9ac9b273a5a69c3daa81623f1a
Suresh <suresh.thorata@gmail.com>
2018-03-01 02:03:11 +0530

    Commit to GitHub

--- SwiftWorld/ViewController.swift
+++ SwiftWorld/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonClicked(_ sender: UIButton){
-        print("This is a Git tutorial")
+        print("You finished")
     }
     
     override func didReceiveMemoryWarning() {
